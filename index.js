d8f2858a0843c72d7fac910aad91b891f4187232
Aikeilol <garena3307@mail.ru>
2021-12-23 10:07:33 +0300

    Binary Addition

---
+++ index.js
@@ -0,0 +1,27 @@
+function addBinary(a, b) {
+  let sum = a + b,
+    binary = [],
+    ost,
+    neOst,
+    end = '';
+
+  do {
+    ost = sum % 2;
+    binary.push(ost);
+    neOst = Math.floor(sum / 2);
+    console.log(neOst);
+    sum = neOst;
+    if (sum == 1) {
+      binary.push(sum);
+    }
+  } while (sum > 1);
+  console.log(binary);
+  binary.reverse();
+  
+  for (let i = 0; i < binary.length; i++) {
+    end += binary[i];
+  }
+  console.log(end);
+}
+
+addBinary(6,6);
