04256b037dd54ab766ee9d4f780365f83d2c73de
Aikeilol <garena3307@mail.ru>
2021-12-26 16:35:58 +0300

    all done

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ function addBinary(a, b) {
     ost = sum % 2;
     binary.push(ost);
     neOst = Math.floor(sum / 2);
-    console.log(neOst);
     sum = neOst;
     if (sum == 1) {
       binary.push(sum);
     }
   } while (sum > 1);
-  console.log(binary);
   binary.reverse();
   
   for (let i = 0; i < binary.length; i++) {
